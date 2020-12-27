94effd4d96a793a65ec7600cfe7b4b35f7db3a64
Syam Kumar <68432794+Prajapathi@users.noreply.github.com>
2020-12-27 15:57:02 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,25 +1,20 @@
+const codebars = document.querySelectorAll('.code');
+const animecodebar = function(){codebars.forEach(function(codebar){
+codebar.style.width = codebar.getAttribute('data')+'%';
+codebar.style.opacity = 1;
+})};
 
-
+const designbars = document.querySelectorAll('.design');
+const animedesignbar = function(){designbars.forEach(function(designbar){
+designbar.style.width = designbar.getAttribute('data')+'%';
+designbar.style.opacity = 1;
+})};
 
 const bars = document.querySelector('.bars');
 const options = { rootMargin : "0px 0px -300px 0px"};
-const observer = new IntersectionObsever(function(){
-    const code = document.querySelector('#code');
-    const codebars = document.querySelectorAll('.code');
-
-    const animecodebar = function(){codebars.forEach(function(codebar){
-    codebar.style.width = codebar.getAttribute('data')+'%';
-    codebar.style.opacity = 1;
-})};
-
-setTimeout(animecodebar, 700);
-
-const designbars = document.querySelectorAll('.design');
-
-const animedesignbar = function(){designbars.forEach(function(designbar){
-    designbar.style.width = designbar.getAttribute('data')+'%';
-    designbar.style.opacity = 1;
-})};
+const observer = new IntersectionObsever(function(entries, observer){
+    animecodebar();
+    animedesignbar();
 },options);
 
 observer.observe("bars");
