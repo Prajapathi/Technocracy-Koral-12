81c13da66de3615b7060d1704e5d405b90ddcec0
Syam Kumar <68432794+Prajapathi@users.noreply.github.com>
2020-12-27 15:51:36 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,10 +1,16 @@
-const code = document.querySelector('#code');
-const codebars = document.querySelectorAll('.code');
 
-const animecodebar = function(){codebars.forEach(function(codebar){
+
+setTimeout(animedesignbar, 500);
+const bars = document.querySelector('.bars');
+const options = { rootMargin : "0px 0px -300px 0px"};
+const observer = new IntersectionObsever(function(){
+    const code = document.querySelector('#code');
+    const codebars = document.querySelectorAll('.code');
+
+    const animecodebar = function(){codebars.forEach(function(codebar){
     codebar.style.width = codebar.getAttribute('data')+'%';
     codebar.style.opacity = 1;
-})}
+})};
 
 setTimeout(animecodebar, 700);
 
@@ -13,7 +19,7 @@ const designbars = document.querySelectorAll('.design');
 const animedesignbar = function(){designbars.forEach(function(designbar){
     designbar.style.width = designbar.getAttribute('data')+'%';
     designbar.style.opacity = 1;
-})}
+})};
+},options);
 
-setTimeout(animedesignbar, 500);
-
+observer.observe("bars");
