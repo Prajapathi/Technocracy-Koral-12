be1120fbbe5974801cef97c705b412d3181fd0b0
Syam Kumar <68432794+Prajapathi@users.noreply.github.com>
2020-12-28 15:29:59 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -10,11 +10,5 @@ designbar.style.width = designbar.getAttribute('data')+'%';
 designbar.style.opacity = 1;
 })};
 
-const bars = document.querySelector('.bars');
-const options = { rootMargin : "0px 0px -300px 0px"};
-const observer = new IntersectionObsever(function(entries, observer){
-    animecodebar();
-    animedesignbar();
-},options);
-
-observer.observe("bars");
+animecodebars();
+animedesignbar();
